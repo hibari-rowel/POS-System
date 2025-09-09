54b8fcb4635c82f93c1af85faac2faee0a119c04
hibari-rowel <rcdpangue@gmail.com>
2025-09-09 09:29:15 +0800

    rename analytics to dashboard added pages to vue route

--- practice-tailwind-vue/src/router/index.js
+++ practice-tailwind-vue/src/router/index.js
@@ -1,9 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import LoginView from '@/components/ModuleComponents/Auth/LoginView.vue';
-import RegisterView from '@/components/ModuleComponents/Auth/RegisterView.vue';
 import PurchaseView from '@/components/ModuleComponents/Purchases/PurchaseView.vue';
-import AnalyticsView from '@/components/ModuleComponents/Analytics/AnalyticsView.vue';
+import DashboardView from '@/components/ModuleComponents/Dashboard/DashboardView.vue';
+import PurchaseHistoryListView from '@/components/ModuleComponents/PurchaseHistory/ListView.vue';
+
+import ProductsListView from '@/components/ModuleComponents/Products/ListView.vue';
+import ProductsCreateView from '@/components/ModuleComponents/Products/CreateView.vue';
+import ProductsShowView from '@/components/ModuleComponents/Products/ShowView.vue';
+import ProductsEditView from '@/components/ModuleComponents/Products/EditView.vue';
+
+import ProductCategoriesListView from '@/components/ModuleComponents/ProductCategories/ListView.vue';
+import ProductCategoriesCreateView from '@/components/ModuleComponents/ProductCategories/CreateView.vue';
+import ProductCategoriesShowView from '@/components/ModuleComponents/ProductCategories/ShowView.vue';
+import ProductCategoriesEditView from '@/components/ModuleComponents/ProductCategories/EditView.vue';
+
+import ReportsListView from '@/components/ModuleComponents/Reports/ListView.vue';
+import ReportsCreateView from '@/components/ModuleComponents/Reports/CreateView.vue';
+import ReportsShowView from '@/components/ModuleComponents/Reports/ShowView.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -12,25 +26,54 @@ const router = createRouter({
             path: '/',
             name: 'login',
             component: LoginView,
-            meta: { requiresAuth: false },
+            meta: { requiresAuth: false, allowedUsers: ['admin', 'staff'] },
         },
         {
-            path: '/register',
-            name: 'register',
-            component: RegisterView,
-            meta: { requiresAuth: false },
+            path: '/dashboard',
+            name: 'dashboard',
+            component: DashboardView,
+            meta: { requiresAuth: true, allowedUsers: ['admin', 'staff'] },
         },
         {
             path: '/purchases',
             name: 'purchases',
             component: PurchaseView,
-            meta: { requiresAuth: false },
+            meta: { requiresAuth: true, allowedUsers: ['admin', 'staff'] },
         },
         {
-            path: '/analytics',
-            name: 'analytics',
-            component: AnalyticsView,
-            meta: { requiresAuth: false },
+            path: '/purchase-history',
+            name: 'purchase-history',
+            component: PurchaseHistoryListView,
+            meta: { requiresAuth: true, allowedUsers: ['admin', 'staff'] },
+        },
+        {
+            path: '/products',
+            meta: { requiresAuth: true, allowedUsers: ['admin',] },
+            children: [
+                { path: '', component: ProductsListView },
+                { path: 'create', component: ProductsCreateView },
+                { path: 'show/:id', component: ProductsShowView },
+                { path: 'edit/:id', component: ProductsEditView },
+            ],
+        },
+        {
+            path: '/product-categories',
+            meta: { requiresAuth: true, allowedUsers: ['admin',] },
+            children: [
+                { path: '', component: ProductCategoriesListView },
+                { path: 'create', component: ProductCategoriesCreateView },
+                { path: 'show/:id', component: ProductCategoriesShowView },
+                { path: 'edit/:id', component: ProductCategoriesEditView },
+            ],
+        },
+        {
+            path: '/reports',
+            meta: { requiresAuth: true, allowedUsers: ['admin', 'staff'] },
+            children: [
+                { path: '', component: ReportsListView },
+                { path: 'create', component: ReportsCreateView },
+                { path: 'show/:id', component: ReportsShowView },
+            ],
         },
     ]
 });
